ca650ec4a8d301215f2b1a91bff53c14cfddeb50
Jiawei Li <1740459051@qq.com>
2020-06-30 21:52:51 +0800

    Create binary_search.cpp

---
+++ algorithm/binary_search.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int binary_search(vector<int> A, int n, int val){
+    int l=0, r=n-1;
+    int mid;
+    while(l<=r){
+        mid = int((l+r)/2);
+        if(A[mid]<val) l=mid+1;
+        if(A[mid]>val) r=mid-1;
+        if(A[mid]==val){
+            while(mid>=0&&A[mid]==val){
+                mid--;
+            }
+            return mid+1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    vector<int> A;
+    int tmp;
+    for(int i=0;i<n;i++){
+        scanf("%d", &tmp);
+        A.push_back(tmp);
+    }
+    
+    int val;
+    scanf("%d", &val);
+    int res = binary_search(A, n, val);
+    printf("%d", res);
+    
+    return 0;
+}
