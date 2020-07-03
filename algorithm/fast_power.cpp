1dc09c9e9a6b782215ff360e3a7dbc19b023dd0f
Jiawei Li <1740459051@qq.com>
2020-07-03 14:06:40 +0800

    Update fast_power.cpp

--- algorithm/fast_power.cpp
+++ algorithm/fast_power.cpp
@@ -1,1 +1,19 @@
-#
+#include <iostream>
+#include <cstdio>
+#define LL long long
+using namespace std;
+
+LL myPow(LL a, LL b, LL p){
+    int res = 1;
+    for(;b;b>>=1, a=(a*a)%p){
+        if(b&1) res = (res*a)%p;
+    }
+    return res;
+}
+
+int main(){
+    LL res;
+    res = myPow(2, 100000000, 10000000);
+    printf("%lld", res);
+    return 0;
+}
