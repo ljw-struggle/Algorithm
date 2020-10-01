e3097ecf7dc052870f0a68fbee08c6f905aba51d
nadimakram99 <42375102+nadimakram99@users.noreply.github.com>
2020-10-01 09:10:02 +0530

    Update io.cpp

--- _language/cpp/io.cpp
+++ _language/cpp/io.cpp
@@ -1,1 +1,18 @@
+#for implementing the data structure and alogorithm the c++ is one of the finest language
 
+using namespace std;
+int main();
+{
+  # uses of some iinbuilt module in c++
+  cout<<pow(2,5)<<endl;    //this will give the power of first element 
+  ///output::  32
+  cout<<sqrt(36);  
+  //output:: 6
+  cout<<round(4.7);
+  ///output:: 5
+  cout<<floor(4.8);
+  //output:: 4
+  cout<<fmax(3,12);
+  //output:: 12
+  cout<<fmin(3,12);
+  //output;;3
